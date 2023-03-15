471d00d05f897cfb3e5c6921ab1a660d70647f3c
Gracie Gabel <GabelG@WLHS.wlwv.K12.or.us>
2023-03-15 09:26:42 -0700

    l

--- StatsTester.java
+++ StatsTester.java
@@ -1,2 +1,15 @@
 public class StatsTester {
+    public static void main(String[] args) {
+        double[] data = {1, 2, 3, 4, 5, 6};
+
+        //create a StatsCalculator object'
+        StatsCalculator myCalculator = new StatsCalculator(data);
+
+        myCalculator.print();
+        myCalculator.sortData();
+        myCalculator.printSorted();
+        myCalculator.printFiveNumberSummary();
+        System.out.println(myCalculator.calculateMean());
+
+    }
 }
